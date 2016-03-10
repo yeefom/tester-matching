687ce9695900686c438eff394fd18ea5dc25fc6e
Yifeng Wang <yeefom@gmail.com>
2016-03-10 11:10:54 -0800

    Add server structure

---
+++ server/controllers/routes.js
@@ -0,0 +1,5 @@
+// var util = require('../utilities/utilities.js');
+
+module.exports = function (app) {
+
+};
---
+++ server/server.js
@@ -0,0 +1,17 @@
+var morgan = require('morgan');
+var body_parser = require('body-parser');
+var express = require('express');
+var app = express();
+
+app.use(morgan('dev'));
+app.use(body_parser.urlencoded({extended : true}));
+app.use(body_parser.json());
+app.use(express.static('client'));
+
+require('./controllers/routes.js')(app);
+
+app.listen(process.env.PORT || 80, function() {
+  console.log('Express server listening on port 80');
+});
+
+module.exports = app;
