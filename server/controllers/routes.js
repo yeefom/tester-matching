8f2543d0cb91ab2c0b083da4e1b22b546fc863be
Yifeng Wang <yeefom@gmail.com>
2016-03-10 19:27:06 -0800

    Add route for GETting data

--- server/controllers/routes.js
+++ server/controllers/routes.js
@@ -1,5 +1,16 @@
-// var util = require('../utilities/utilities.js');
-
 module.exports = function (app) {
+  var data;
 
+  app.get('/api/data', function (req, res) {
+    if (!data) {
+      // parse data if this is the first API call ever
+      require('../utilities/data.js')()
+      .then(function (parsedData) {
+        data = parsedData;
+        res.status(200).send(data);
+      });
+    } else {
+      res.status(200).send(data);
+    }
+  });
 };
