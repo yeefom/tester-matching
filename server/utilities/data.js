cdc252a33cabddc6011a3fce843d72463b5e4606
Yifeng Wang <yeefom@gmail.com>
2016-03-10 19:27:37 -0800

    Replace fs with readFile helper

--- server/utilities/data.js
+++ server/utilities/data.js
@@ -1,54 +1,68 @@
-var promisify = require("promisify-node");
-var fs = promisify("fs");
-var parser = require('./parser.js');
+var util = require('./utilities.js');
 
-var output = [];
-var devices = [];
+module.exports = function () {
+  var testers = [];
+  var devices = [];
+  var countries = {};
 
-fs.readFile(parser.pathParser('../data/testers.csv'))
-.then(function (data) {
-  return parser.csvParser(data);
-})
-.then(function (data) {
-  for (var i = 1; i < data.length; i++) {
-    output[i - 1] = {
-      id: data[i][0],
-      name: data[i][1] + ' ' + data[i][2],
-      country: data[i][3],
-      devices: {}
-    };
-  }
-})
-.then(function () {
-  return fs.readFile(parser.pathParser('../data/devices.csv'));
-})
-.then(function (data) {
-  return parser.csvParser(data);
-})
-.then(function (data) {
-  for (var i = 1; i < data.length; i++) {
-    devices[i - 1] = data[i][1];
-  }
-})
-.then(function () {
-  return fs.readFile(parser.pathParser('../data/tester_device.csv'));
-})
-.then(function (data) {
-  return parser.csvParser(data);
-})
-.then(function (data) {
-  for (var i = 1; i < data.length; i++) {
-    var tester = data[i][0] - 1;
-    var deviceName = devices[data[i][1] - 1];
-    output[tester].devices[deviceName] = 0;
-  }
-})
-.then(function () {
-  console.log(output);
-  // read bug file
-})
-.catch(function (err) {
-  console.error('ERR in parsing data', err);
-});
-
-module.exports = output;
+  return util.readFile('../data/testers.csv')
+  .then(function (data) {
+    return util.csvParser(data);
+  })
+  .then(function (data) {
+    var country;
+    for (var i = 1; i < data.length; i++) {
+      country = data[i][3];
+      if (!countries[country]) {
+        countries[country] = true;
+      }
+      testers[i - 1] = {
+        id: data[i][0],
+        name: data[i][1] + ' ' + data[i][2],
+        country: country,
+        devices: {}
+      };
+    }
+  })
+  .then(function () {
+    return util.readFile('../data/devices.csv');
+  })
+  .then(function (data) {
+    return util.csvParser(data);
+  })
+  .then(function (data) {
+    for (var i = 1; i < data.length; i++) {
+      devices[i - 1] = data[i][1];
+    }
+  })
+  .then(function () {
+    return util.readFile('../data/tester_device.csv');
+  })
+  .then(function (data) {
+    return util.csvParser(data);
+  })
+  .then(function (data) {
+    for (var i = 1; i < data.length; i++) {
+      var tester = data[i][0] - 1;
+      var deviceName = devices[data[i][1] - 1];
+      testers[tester].devices[deviceName] = 0;
+    }
+  })
+  .then(function () {
+    return util.readFile('../data/bugs.csv');
+  })
+  .then(function (data) {
+    return util.csvParser(data);
+  })
+  .then(function (data) {
+    for (var i = 1; i < data.length; i++) {
+      var tester = data[i][2] - 1;
+      var deviceName = devices[data[i][1] - 1];
+      testers[tester].devices[deviceName]++;
+    }
+    return {testers: testers, countries: countries, devices: devices};
+  })
+  .catch(function (err) {
+    console.error('ERR in parsing data', err);
+  });
+};
