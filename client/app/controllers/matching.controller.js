2b89f42a2b0fa580cbdb1e72806bddd83ff8b08f
Yifeng Wang <yeefom@gmail.com>
2016-03-11 16:52:29 -0800

    Add controller for matching page

--- client/app/controllers/matching.controller.js
+++ client/app/controllers/matching.controller.js
@@ -5,9 +5,32 @@
       .module('testermatching')
       .controller('matchingCtrl', matchingCtrl);
 
-    matchingCtrl.$inject = ['$scope'];
+    matchingCtrl.$inject = ['$scope', 'dataService'];
 
-    function matchingCtrl($scope) {
+    function matchingCtrl($scope, dataService) {
+      $scope.testers = {};
+      $scope.countries = {};
+      $scope.devices = {};
 
+      $scope.getData = function () {
+        dataService.getData()
+        .then(function (data) {
+          $scope.testers.data = data.testers;
+          $scope.countries.data = data.countries;
+          $scope.devices.data = data.devices;
+          console.log($scope.devices.data);
+          console.log($scope.countries.data);
+        });
+      };
+
+      $scope.selectCountry = function (country, selected) {
+        console.log(country, selected);
+      };
+
+      $scope.selectDevice = function (device) {
+        
+      };
+
+      $scope.getData();
     }
 })();
