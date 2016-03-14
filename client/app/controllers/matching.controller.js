788b1a4e068770a9fe79515804ef207aac95ab5e
Yifeng Wang <yeefom@gmail.com>
2016-03-14 10:56:28 -0700

    Add manual filter

--- client/app/controllers/matching.controller.js
+++ client/app/controllers/matching.controller.js
@@ -8,29 +8,93 @@
     matchingCtrl.$inject = ['$scope', 'dataService'];
 
     function matchingCtrl($scope, dataService) {
-      $scope.testers = {};
-      $scope.countries = {};
-      $scope.devices = {};
-
-      $scope.getData = function () {
-        dataService.getData()
-        .then(function (data) {
-          $scope.testers.data = data.testers;
-          $scope.countries.data = data.countries;
-          $scope.devices.data = data.devices;
-          console.log($scope.devices.data);
-          console.log($scope.countries.data);
-        });
+      var testers;
+      var criteria = {
+        country: {},
+        countrySize: 0,
+        device: {},
+        deviceSize: 0
       };
 
+      $scope.countries = {};
+      $scope.devices = {};
+      $scope.testersMatched = [];
+
       $scope.selectCountry = function (country, selected) {
-        console.log(country, selected);
+        if (selected) {
+          $scope.criteria.country[country] = true;
+          $scope.criteria.countrySize++;
+        } else {
+          delete $scope.criteria.country[country];
+          $scope.criteria.countrySize--;
+        }
+        $scope.render();
       };
 
       $scope.selectDevice = function (device) {
         
       };
 
-      $scope.getData();
+      function render () {
+        var shown = [];
+        var testerMatched;
+        if (!criteria.countrySize && !criteria.deviceSize) {
+          for (var i = 0; i < testers.length; i++) {
+            testerMatched = fillBasicInfo(testers, i);
+            testerMatched.bugsFiled = 0;
+            for (var prop in testerMatched.devices) {
+              testerMatched.bugsFiled += testerMatched.devices[prop];
+            }
+            shown.push(testerMatched);
+          }
+        } else if (!$scope.criteria.countrySize) {
+          var isPicked;
+          var bugsFiled; 
+          for (var i = 0; i < testers; i++) {
+            isPicked = false;
+            for (var prop in criteria.device) {
+              bugsFiled = testers[i].devices[prop];
+              if (!isPicked) {
+                if (bugsFiled){
+                  isPicked = true;
+                  testerMatched = fillBasicInfo(testers, i);
+                  testerMatched.devices[prop] = bugsFiled;
+                  testerMatched.bugsFiled = bugsFiled;
+                }
+              } else {
+                if (bugsFiled){
+                  testerMatched.devices[prop] = bugsFiled;
+                  testerMatched.bugsFiled += bugsFiled;
+                }
+              }
+            }
+          }
+        } else if (!$scope.criteria.deviceSize) {
+
+        }
+        else {
+
+        }
+        $scope.testersMatched = shown;
+      }
+
+      function getData () {
+        dataService.getData()
+        .then(function (data) {
+          testers = data.testers;
+          $scope.countries.data = data.countries;
+          $scope.devices.data = data.devices;
+        });
+      }
+
+      function fillBasicInfo (testers, i) {
+        var testerMatched = {};
+        testerMatched.name = testers[i].name;
+        testerMatched.country = testers[i].country;
+        return testerMatched;
+      }
+
+      getData();
+      $scope.render();
     }
 })();
