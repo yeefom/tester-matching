f4cc616e1ad1fa3b1d313cda7c1097e2e7450cf0
Yifeng Wang <yeefom@gmail.com>
2016-03-14 15:30:01 -0700

    Add match service

---
+++ client/app/services/match.service.js
@@ -0,0 +1,87 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('testermatching')
+        .service('matchService', matchService);
+
+    matchService.$inject = [];
+
+    function matchService() {
+      var parseAllDevices = function (testerMatched, tester, criteria) {
+        testerMatched.devices = tester.devices;
+        testerMatched.bugsFiled = 0;
+        for (var prop in testerMatched.devices) {
+          testerMatched.bugsFiled += testerMatched.devices[prop];
+        }
+      };
+
+      var parseSelectedDevices = function (testerMatched, tester, criteria) {
+        var bugsFiled;
+        var isPicked = false;
+        for (var prop in criteria.device) {
+          bugsFiled = tester.devices[prop];
+          if (bugsFiled) {
+            if(!isPicked) {
+              testerMatched.devices = {};
+              testerMatched.devices[prop] = bugsFiled;
+              testerMatched.bugsFiled = bugsFiled;
+              isPicked = true;
+            } else {
+              testerMatched.devices[prop] = bugsFiled;
+              testerMatched.bugsFiled += bugsFiled;
+            }
+          }
+        }
+        return isPicked;
+      };
+
+      var TesterMatched = function (name, country) {
+        this.name = name;
+        this.country = country;
+      };
+
+      var findMatches = function (testers, criteria) {
+        var shown = [];
+        var testerMatched;
+        if (!criteria.countrySize && !criteria.deviceSize) {
+          for (var i = 0; i < testers.length; i++) {
+            testerMatched = new TesterMatched(testers[i].name, testers[i].country);
+            parseAllDevices(testerMatched, testers[i], criteria);
+            shown.push(testerMatched);
+          }
+        } else if (!criteria.countrySize) {
+          var bugsFiled;
+          for (var i = 0; i < testers.length; i++) {
+            testerMatched = new TesterMatched(testers[i].name, testers[i].country);
+            if(parseSelectedDevices(testerMatched, testers[i], criteria)) {
+              shown.push(testerMatched);
+            }
+          }
+        } else if (!criteria.deviceSize) {
+          for (var i = 0; i < testers.length; i++) {
+            if (criteria.country[testers[i].country]) {
+              testerMatched = new TesterMatched(testers[i].name, testers[i].country);
+              parseAllDevices(testerMatched, testers[i], criteria);
+              shown.push(testerMatched);
+            }
+          }
+        }
+        else {
+          for (var i = 0; i < testers.length; i++) {
+            if(criteria.country[testers[i].country]) {
+              testerMatched = new TesterMatched(testers[i].name, testers[i].country);
+              if (parseServiceparseSelectedDevices(testerMatched, testers[i], criteria)) {
+                shown.push(testerMatched);
+              }
+            }
+          }
+        }
+        return shown;
+      };
+
+      return {
+        findMatches: findMatches
+      };
+    }
+})();
