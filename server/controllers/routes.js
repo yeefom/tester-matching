243fdf626092d5c886d6ab04dc3cc88a21a45ccd
Yifeng Wang <yeefom@gmail.com>
2016-03-14 21:13:19 -0700

    Add brief documentation

--- server/controllers/routes.js
+++ server/controllers/routes.js
@@ -3,7 +3,7 @@ module.exports = function (app) {
 
   app.get('/api/data', function (req, res) {
     if (!data) {
-      // parse data if this is the first API call ever
+      // parse and store data if this is the first API call ever
       require('../utilities/data.js')()
       .then(function (parsedData) {
         data = parsedData;
