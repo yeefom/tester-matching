e2d59e8ab3817faa78696526d54801d6c3cf0ec4
Yifeng Wang <yeefom@gmail.com>
2016-03-17 15:08:23 -0700

    Clean code

--- client/app/controllers/matching.controller.js
+++ client/app/controllers/matching.controller.js
@@ -12,95 +12,73 @@
       var criteria = {
         country: {},
         countrySize: 0,
+        countryMaxSize: 0,
         device: {},
-        deviceSize: 0
+        deviceSize: 0,
+        deviceMaxSize: 0
       };
-      var countriesLength;
-      var devicesLength;
 
-      $scope.countries = {};
-      $scope.devices = {};
+      $scope.country = {};
+      $scope.device = {};
       $scope.testersMatched = [];
 
       $scope.countryAll = {selected: false};
       $scope.deviceAll = {selected: false};
 
-      $scope.select = function (type, item, selected) {      
-        if (type === 'country') {
-          if ($scope.countryAll.selected) {
-            $scope.countryAll.selected = false;
+      var selectOne = function (type) {
+        var typeAll = $scope[type + 'All'];
+        return function (item, selected) {
+          if (typeAll.selected) {
+            typeAll.selected = false;
           }
           if (selected) {
-            criteria.country[item] = true;
-            criteria.countrySize++;
-            if (criteria.countrySize === countriesLength) {
-              $scope.countryAll.selected = true;
+            criteria[type][item] = true;
+            criteria[type + 'Size']++;
+            if (criteria[type + 'Size'] === criteria[type + 'MaxSize']) {
+              typeAll.selected = true;
             }
           } else {
-            delete criteria.country[item];
-            criteria.countrySize--;
+            delete criteria[type][item];
+            criteria[type + 'Size']--;
           }
-        } else {
-          if ($scope.deviceAll.selected) {
-            $scope.deviceAll.selected = false;
-          }
-          if (selected) {
-            criteria.device[item] = true;
-            criteria.deviceSize++;
-            if (criteria.deviceSize === devicesLength) {
-              $scope.deviceAll.selected = true;
-            }
-          } else {
-            delete criteria.device[item];
-            criteria.deviceSize--;
-          }
-        }
-        $scope.testersMatched = matchService.findMatches(testers, criteria);
+          $scope.testersMatched = matchService.findMatches(testers, criteria);
+        };
       };
 
-      $scope.selectAll = function (type, selected) {
-        if (selected) {
-          if (type === 'country') {
-            criteria.countrySize = 0;
-            for (var prop in $scope.countries.data) {
-              $scope.countries.data[prop] = true;
-              criteria.country[prop] = true;
-              criteria.countrySize++;
+      $scope.selectCountry = selectOne('country');
+      $scope.selectDevice = selectOne('device');
+
+      var selectAll = function (type) {
+        var collection = $scope[type];
+        return function (selected) {
+          if (selected) {
+            criteria[type + 'Size'] = criteria[type + 'MaxSize'];
+            for (var prop in collection.data) {
+              collection.data[prop] = true;
+              criteria[type][prop] = true;
             }
           } else {
-            criteria.deviceSize = 0;
-            for (var prop in $scope.devices.data) {
-              $scope.devices.data[prop] = true;
-              criteria.device[prop] = true;
-              criteria.deviceSize++;
+            criteria[type + 'Size'] = 0;
+            for (var prop in collection.data) {
+              collection.data[prop] = false;
+              delete criteria[type][prop];
             }
           }
-        } else {
-          if (type === 'country') {
-            criteria.countrySize = 0;
-            for (var prop in $scope.countries.data) {
-              $scope.countries.data[prop] = false;
-              delete criteria.country[prop];
-            }
-          } else {
-            criteria.deviceSize = 0;
-            for (var prop in $scope.devices.data) {
-              $scope.devices.data[prop] = false;
-              delete criteria.device[prop];
-            }
-          }
-        }
-        $scope.testersMatched = matchService.findMatches(testers, criteria);
+          $scope.testersMatched = matchService.findMatches(testers, criteria);
+        };
       };
+
+      $scope.selectAllCountries = selectAll('country');
+      $scope.selectAllDevices = selectAll('device');
 
       var getData = function () {
         dataService.getData()
         .then(function (data) {
           testers = data.testers;
-          $scope.countries.data = data.countries;
-          countriesLength = Object.keys($scope.countries.data).length;
-          $scope.devices.data = data.devices;
-          devicesLength = Object.keys($scope.devices.data).length;
+          $scope.country.data = data.countries;
+          criteria.countryMaxSize = Object.keys($scope.country.data).length;
+          $scope.device.data = data.devices;
+          criteria.deviceMaxSize = Object.keys($scope.device.data).length;
           $scope.testersMatched = matchService.findMatches(testers, criteria);
         });
       };
